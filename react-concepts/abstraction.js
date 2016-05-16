b3c8cdcef507fe935b737c4c9893e4ebc7f78265
vasanthk <vasa.httpster@gmail.com>
2016-05-15 17:20:25 -0700

    Abstraction

---
+++ react-concepts/abstraction.js
@@ -0,0 +1,24 @@
+/**
+ * You can't fit a complex UI in a single function though.
+ * It is important that UIs can be abstracted into reusable pieces that don't leak their implementation details.
+ * Such as calling one function from another.
+ */
+
+function FancyUserBox(user) {
+    return {
+        borderStyle: '1px solid blue',
+        childContent: [
+            'Name: ',
+            NameBox(user.firstName + ' ' + user.lastName)
+        ]
+    };
+}
+
+//{ firstName: 'Vasa', lastName: 'K' } ->
+//{
+//    borderStyle: '1px solid blue',
+//    childContent: [
+//      'Name: ',
+//      { fontWeight: 'bold', labelContent: 'Vasa K' }
+//    ]
+//};
